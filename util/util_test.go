bc6d8ad89adb35146582a163f434860ada46d6e0
bdular <blaz.dular@gmail.com>
2024-07-29 22:23:10 +0200

    Added util package tests

--- util/util_test.go
+++ util/util_test.go
@@ -1,37 +1,27 @@
 package util
 
 import (
-	"reflect"
 	"testing"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestStringToObjectId(t *testing.T) {
-	// Test valid input
-	validInput := []string{
-		"5f6b9e9e56a0f0a3c8d17a91",
-		"5f6b9e9e56a0f0a3c8d17a92",
-		"5f6b9e9e56a0f0a3c8d17a93",
-	}
-	expected := []primitive.ObjectID{
-		{0x5f, 0x6b, 0x9e, 0x9e, 0x56, 0xa0, 0xf0, 0xa3, 0xc8, 0xd1, 0x7a, 0x91},
-		{0x5f, 0x6b, 0x9e, 0x9e, 0x56, 0xa0, 0xf0, 0xa3, 0xc8, 0xd1, 0x7a, 0x92},
-		{0x5f, 0x6b, 0x9e, 0x9e, 0x56, 0xa0, 0xf0, 0xa3, 0xc8, 0xd1, 0x7a, 0x93},
-	}
+type exampleInterface interface {
+	ExampleMethod()
+}
 
-	result, _ := StringToObjectId(validInput...)
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("expected %v, got %v", expected, result)
-	}
+type exampleStructImplementsInterface struct {
+}
 
-	// Test invalid input
-	invalidInput := []string{
-		"invalid_id",
-		"also_invalid",
-	}
-	result, _ = StringToObjectId(invalidInput...)
-	if len(result) != 0 {
-		t.Errorf("expected empty slice, got %v", result)
-	}
+func (e exampleStructImplementsInterface) ExampleMethod() {
+}
+
+func TestIsNilInterfaceOrPointer(t *testing.T) {
+	// Check if pointer is nil
+	pointer := new(int)
+	assert.False(t, IsNilInterfaceOrPointer(pointer))
+	assert.True(t, IsNilInterfaceOrPointer(nil))
+
+	// Check if interface is nil
+	assert.False(t, IsNilInterfaceOrPointer(&exampleStructImplementsInterface{}))
 }
